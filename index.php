f4270ee75869d659b62db15ad296be144164c373
Zunaeed-Imran <zunaeede@gmail.com>
2023-08-20 12:39:33 +0600

    main -foreach use

--- index.php
+++ index.php
@@ -58,4 +58,10 @@ echo "hello";
 //   echo $i."<br>";
 // }
 
+
+// $asso = ["iron_man1" => 2008, "iron_man2" => 2010, "iron_man3" => 2014];
+// foreach($asso as $var1){
+//   echo  $var1."<br>";
+// }
+
 ?>
