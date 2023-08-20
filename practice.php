13b199c5ed71184a56425fd72c3877e4994b2d69
Zunaeed-Imran <zunaeede@gmail.com>
2023-08-20 12:56:24 +0600

    main -nasic form

---
+++ practice.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="practice.php" method="get">
+    <input type="text" name="name" placeholder="enter name">
+    <button type="submit">Submit</button>
+  </form>
+</body>
+</html>
+
+<?php
+if(isset($_GET['name'])){
+  echo "Hello"."  ". $_GET['name'];
+}
+
+?>
