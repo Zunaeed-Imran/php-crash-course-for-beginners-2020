75cb117ddfc24ff54f4066c899e8b2978ade1ef7
imran <zunaeede@gmail.com>
2023-08-23 16:36:38 +0600

    main -if else

--- 3_if_else.php
+++ 3_if_else.php
@@ -1,5 +1,16 @@
 <?php
 
-echo "HEllo world";
+$a = 5;
+$b = 50;
+
+
+if($a > $b){
+  echo "a is a big number";
+}elseif($a === $b){
+  echo "a and b is same number";
+}else{
+  echo "a is small number";
+}
+
 
 ?>
